4bad89d87ed3687d8d81229a8430a1ad9c55970b
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2024-03-07 19:48:58 +0100

    groupes

--- src/app/shared/services/groupe.service.ts
+++ src/app/shared/services/groupe.service.ts
@@ -1,9 +1,27 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GroupeService {
+  url = '/api';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  
+  addgroupe(groupe: any) {
+    console.log(groupe);
+    // Post data or return error
+    return this.http.post(`${this.url}/groupe`, groupe);
+  }
+
+  getGroupes() {
+    // Get data or return error
+    return this.http.get(`${this.url}/groupe`);
+  }
+  
+  deleteGroupe(id: number) {
+    // Delete data or return error
+    return this.http.delete(`${this.url}/groupe/${id}`);
+  }
 }
--- src/app/views/theme/typography.component.ts
+++ src/app/views/theme/typography.component.ts
@@ -1,8 +1,61 @@
 import { Component } from '@angular/core';
+import { UserService } from '../../shared/services/user.service';
+import { GroupeService } from '../../shared/services/groupe.service';
+import { get } from 'lodash-es';
+import { Groupe } from 'src/app/shared/interfaces/groupe';
 
 @Component({
   templateUrl: 'typography.component.html',
 })
 export class TypographyComponent {
-  constructor() {}
+users:any;
+groupes:any;
+groupe={} as Groupe;
+  constructor(private userService:UserService , private groupesService:GroupeService) {
+    console.log("ok");
+this.getGroupes();
+this.getUsers();
+
+  }
+  getUsers()
+  {
+    this.userService.getUsers().subscribe(
+      (data:any)=>{
+        console.log("users:",data);
+        this.users=data;
+      });
+  }
+  getGroupes()
+  {
+    this.groupesService.getGroupes().subscribe(
+      (data:any)=>{
+        console.log("groupes:",data);
+        this.groupes=data;
+      });
+  }
+  deleteUser(us:any)
+  {
+    if(confirm("Vous voulez supprimer "+us.nom+" "+us.prenom))
+    {
+    this.userService.deleteUser(us.id).subscribe(
+      (data:any)=>{
+        console.log(data);
+        this.getUsers();
+      });
+    }
+  }
+  addGroupe()
+  {
+    let gr=Object.assign({},this.groupe);
+    console.log("groupe",gr);
+    this.groupesService.addgroupe(gr).subscribe(
+      (data:any)=>{
+        console.log(data);
+        this.getGroupes();
+        this.groupe={} as Groupe;
+      });
+  }
+
+
+
 }
